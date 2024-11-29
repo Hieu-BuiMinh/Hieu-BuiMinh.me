dfcd2bdd6f3f9239bad1df83503d0e8708d185fe
HieuBuiMinh <85927165+Hieu-BuiMinh@users.noreply.github.com>
2024-11-29 17:43:25 +0700

    main: fix grid columns error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./view/**/*.{js,ts,jsx,tsx,mdx}',
+		'./content/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
