e3ef8f2241f77b13f85bea2a024fefb77952953c
Hieu-BuiMinh <minhhieu2122000@gmail.com>
2025-01-17 01:22:20 +0700

    main: add new content, add vercel blob file storage

--- view/route/about/pages/about.page.tsx
+++ view/route/about/pages/about.page.tsx
@@ -2,14 +2,14 @@ import { notFound } from 'next/navigation'
 
 import { pages } from '@/.velite'
 import { MDXContent } from '@/components/commons/mdx'
-import Hexagram from '@/components/commons/mdx/custom-components/yi-jing/hexagram'
+// import Hexagram from '@/components/commons/mdx/custom-components/yi-jing/hexagram'
 import PageTitle from '@/view/components/blog-content/page-title'
 import PostLastUpdated from '@/view/components/blog-content/post-last-updated'
 
 const title = 'About'
 const description = '👋 Hi there! I am Hieu, a learner who loves web development.'
 
-export default function AboutPageView() {
+export default async function AboutPageView() {
 	const post = pages.find((post) => post.slugAsParams === 'about')
 
 	if (!post || !post.published) {
@@ -20,166 +20,6 @@ export default function AboutPageView() {
 		<div className="container flex max-w-4xl flex-col gap-3">
 			<PageTitle title={title} description={description} />
 
-			{/* <Hexagram
-				showIndex
-				showSixRelatives
-				showElements
-				showBranches
-				showSixCreatures
-				upper={1}
-				lower={1}
-				showOriginFamily
-				showResultHexagram
-				showQuestionerAndQuestion
-				actives={[1, 2, 3, 4, 5, 6]}
-			/> */}
-
-			<div className="flex flex-wrap items-center justify-center gap-4">
-				{/* <Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={4}
-					lower={2}
-					actives={[6]}
-				/> */}
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={1}
-					lower={1}
-				/>
-				{/* <Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={1}
-					lower={5}
-					actives={[1]}
-				/> */}
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={1}
-					lower={7}
-					actives={[2]}
-				/>
-				{/* <Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={1}
-					lower={8}
-					actives={[3]}
-				/>
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={5}
-					lower={8}
-					actives={[4]}
-				/>
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={7}
-					lower={8}
-					actives={[5]}
-				/>
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={3}
-					lower={8}
-					actives={[4]}
-				/>
-				<Hexagram
-					showOriginFamily
-					showHexagramName
-					showBranches
-					showElements
-					showIndex
-					showLabel
-					showQuestionerAndQuestion
-					showResultHexagram
-					showHiddenRelative
-					showSixCreatures
-					showSixRelatives
-					upper={3}
-					lower={1}
-					actives={[1, 2, 3]}
-				/> */}
-			</div>
-
 			<article className="container prose mx-auto max-w-3xl pb-6 dark:prose-invert">
 				<MDXContent code={post.body} />
 				{post.lastUpdated && <PostLastUpdated date={post.lastUpdated} />}
@@ -187,7 +27,3 @@ export default function AboutPageView() {
 		</div>
 	)
 }
-/*
-i 0 1 2 3 4 5
-q 6 5 4 3 2 1
-*/
